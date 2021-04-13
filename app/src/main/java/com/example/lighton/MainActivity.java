d8592a4f75d1a5d77cd4d654c5249976dd750186
rihammastour <48338432+rihammastour@users.noreply.github.com>
2021-04-13 06:51:38 +0300

    passcode and timer features DONE ✔️- fix Register UX, change home background.

--- app/src/main/java/com/example/lighton/MainActivity.java
+++ app/src/main/java/com/example/lighton/MainActivity.java
@@ -28,4 +28,10 @@ public class MainActivity extends AppCompatActivity {
         db.delete();
         finish();
     }
+
+    public void goToTimerPasscode(View v){
+
+        Intent intent = new Intent(this, TimerPasscode.class);
+        startActivity(intent);
+    }
 }
