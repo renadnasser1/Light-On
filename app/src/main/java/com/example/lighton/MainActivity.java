c2962df03693a7f57404ccbef8b9f9b5642b3f3c
AreejAljeraiwi <48260156+AreejAljeraiwi@users.noreply.github.com>
2021-04-16 05:56:00 +0300

    get location not work

--- app/src/main/java/com/example/lighton/MainActivity.java
+++ app/src/main/java/com/example/lighton/MainActivity.java
@@ -3,9 +3,14 @@ package com.example.lighton;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.se.omapi.Session;
@@ -14,8 +19,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.net.Authenticator;
 import java.net.PasswordAuthentication;
 import java.util.Properties;
@@ -24,13 +31,13 @@ import javax.mail.MessagingException;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+
 import android.util.Log;
 
 import java.util.Date;
 import java.util.Properties;
 
 import javax.mail.Message;
-
 
 
 import java.io.UnsupportedEncodingException;
@@ -49,6 +56,7 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
+
 import java.security.Security;
 import java.util.Properties;
 
@@ -60,6 +68,7 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import javax.mail.util.ByteArrayDataSource;
+
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.se.omapi.Session;
@@ -69,7 +78,14 @@ import android.widget.Button;
 import android.widget.EditText;
 //import javax.mail;
 import androidx.appcompat.app.AppCompatActivity;
-import java.net.* ;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.net.*;
 import java.net.Authenticator;
 import java.net.PasswordAuthentication;
 import java.util.*;
@@ -80,6 +96,12 @@ public class MainActivity extends AppCompatActivity {
     EditText etTo, etSubject, etMessage;
     Button btSend;
     String sEmail, sPassword;
+
+    //location varibles
+    Button btLocation;
+    TextView textView1, textView2, textView3, textView4, textView5;
+    FusedLocationProviderClient fusedLocationProviderClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -92,24 +114,53 @@ public class MainActivity extends AppCompatActivity {
         etMessage = findViewById(R.id.et_message);
         btSend = findViewById(R.id.bt_send);
 
+        //Assign location variable
+        btLocation = findViewById(R.id.bt_location);
+        textView1 = findViewById(R.id.text_view1);
+        textView2 = findViewById(R.id.text_view2);
+        textView3 = findViewById(R.id.text_view3);
+        textView4 = findViewById(R.id.text_view4);
+        textView5 = findViewById(R.id.text_view5);
+
+
+        //initalize fusedLocationProviderClient
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        btLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Areej tag", "statrt on click :" + toString());
+
+                //check premeisson
+                if (ActivityCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    //when premission graned
+                    getLocation();
+                } else {
+                    //when permission denied
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+                }
+            }
+        });
+
+
         //Sender email credential
         sEmail = "raghadalfheed76@gmail.com";
-       // char[] sPassword = {'p','a','s','s'};
+        // char[] sPassword = {'p','a','s','s'};
         String sPassword = "Raghadalfheed_7676";
 
 
         btSend.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
 
                 Properties properties = new Properties();
-              properties.put("mail.smtp.auth", "true");
+                properties.put("mail.smtp.auth", "true");
                 properties.put("mail.smtp.starttls.enable", "true");
                 properties.put("mail.smtp.host", "smtp.gmail.com");
-               // smtp.googlemail.com
-                properties.put("mail.smtp.port",  "587");
-            // properties.put("mail.smtp.user", sEmail);
-               // properties.put("mail.smtp.socketFactory.fallback", "fallback");
+                // smtp.googlemail.com
+                properties.put("mail.smtp.port", "587");
+                // properties.put("mail.smtp.user", sEmail);
+                // properties.put("mail.smtp.socketFactory.fallback", "fallback");
 //                properties.put("mail.smtp.EnableSSL.enable", "true");
 //                properties.put("mail.smtp.socketFactory.port", "465");
 //                properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
@@ -117,7 +168,7 @@ public class MainActivity extends AppCompatActivity {
                 javax.mail.Session session = javax.mail.Session.getInstance(properties, new javax.mail.Authenticator() {
                     @Override
                     protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
-                        return new javax.mail.PasswordAuthentication(sEmail,sPassword);
+                        return new javax.mail.PasswordAuthentication(sEmail, sPassword);
                     }
                 });
 
@@ -129,7 +180,7 @@ public class MainActivity extends AppCompatActivity {
                     //Sender email
                     message.setFrom(new InternetAddress(sEmail));
                     //recipient email
-                    message.setRecipients(Message.RecipientType.TO,InternetAddress.parse(etTo.getText().toString().trim()));
+                    message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(etTo.getText().toString().trim()));
 
                     //Email subject
                     message.setSubject(etSubject.getText().toString().trim());
@@ -138,16 +189,94 @@ public class MainActivity extends AppCompatActivity {
 
                     //send email
                     new SendMail().execute(message);
-                }catch (MessagingException e){
+                } catch (MessagingException e) {
                     e.printStackTrace();
-                } catch (Exception e){
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
 
 
             }
-            });
+        });
+    }
+
+    private void getLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
         }
+        fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                //initalize location
+                Log.d("Areej tag", "statrt on cmplete :" + toString());
+
+                Location location = task.getResult();
+                if(location == null){
+                    Log.d("Areej tag", "اللوكيشن  نل :" + toString());
+
+                }
+                if(location != null){
+                    Log.d("Areej tag", "اللوكيشن مب نل :" + toString());
+
+                    try {
+                        Log.d("Areej tag", "دخلت التراي :" + toString());
+
+                        // inilize geoCoder
+                        Geocoder geocoder = new Geocoder(MainActivity.this, Locale.getDefault());
+                        Log.d("Areej tag", "سويت اوبجكت جي كودر :" + toString());
+
+                        //inilize address list
+                        List<Address> addresses = geocoder.getFromLocation(
+                                location.getLatitude(),location.getLongitude(), 1
+                        );
+                        Log.d("Areej tag", "سويت اللست :" + toString());
+
+                        //set latitude on TextView
+                        textView1.setText(Html.fromHtml("<font color='#6200EE'><b>Latitude :</b><br></font>"
+                        + addresses.get(0).getLatitude()));
+                        textView2.setText(Html.fromHtml(
+                                "<font color='#6200EE'><b>Longitude :</b><br></font>"
+                                        + addresses.get(0).getLongitude()
+                        ));
+                        //Set country name
+                        textView3.setText(Html.fromHtml(
+                                "<font color='#6200EE'><b>Country Name :</b><br></font>"
+                                        + addresses.get(0).getCountryName()
+                        ));
+                        //Set locality
+                        textView4.setText(Html.fromHtml(
+                                "<font color='#6200EE'><b>Locality :</b><br></font>"
+                                        + addresses.get(0).getLocality()
+                        ));
+                        //set address
+                        textView5.setText(Html.fromHtml(
+                                "<font color='#6200EE'><b>Address :</b><br></font>"
+                                        + addresses.get(0).getAddressLine(0)
+                        ));
+
+                        Log.d("Areej tag", "Yaaaaaaaay :" + toString());
+
+                    } catch (IOException e) {
+                        Log.d("Areej tag", "ERRRROR raghaaaaaaaad :" + toString());
+
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        });
+    }
+
+
+
+
 
     private class SendMail extends AsyncTask<javax.mail.Message,String,String> {
 
