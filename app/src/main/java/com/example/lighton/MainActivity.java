73d67310049765102e8e149774baf6db13ed2b0e
raghad <raghadalfheed76@gmail.com>
2021-04-17 03:17:34 +0300

    all it is work الحمدلله

--- app/src/main/java/com/example/lighton/MainActivity.java
+++ app/src/main/java/com/example/lighton/MainActivity.java
@@ -1,8 +1,5 @@
 package com.example.lighton;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.Manifest;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
@@ -13,70 +10,15 @@ import android.location.Geocoder;
 import android.location.Location;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.se.omapi.Session;
 import android.text.Html;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.IOException;
-import java.net.Authenticator;
-import java.net.PasswordAuthentication;
-import java.util.Properties;
-
-import javax.mail.MessagingException;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-
-import android.util.Log;
-
-import java.util.Date;
-import java.util.Properties;
-
-import javax.mail.Message;
-
-
-import java.io.UnsupportedEncodingException;
-import java.util.Date;
-
-import javax.activation.DataHandler;
-import javax.activation.DataSource;
-import javax.activation.FileDataSource;
-import javax.mail.BodyPart;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Multipart;
-
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
-import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
-
-import java.security.Security;
-import java.util.Properties;
-
-import javax.activation.DataHandler;
-
-import javax.mail.Message;
-
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-import javax.mail.util.ByteArrayDataSource;
-
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.se.omapi.Session;
-
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-//import javax.mail;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
@@ -85,18 +27,30 @@ import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
-import java.net.*;
-import java.net.Authenticator;
-import java.net.PasswordAuthentication;
-import java.util.*;
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+//import javax.mail;
 
 public class MainActivity extends AppCompatActivity {
     //Initialize varible
     EditText etTo, etSubject, etMessage;
     Button btSend;
     String sEmail, sPassword;
-
+    String locationCountry;
+    String locationCity;
+    String locationAddress;
+    double locationLatitude;
+    double locationLongitude;
+    Properties properties = new Properties();
     //location varibles
     Button btLocation;
     TextView textView1, textView2, textView3, textView4, textView5;
@@ -121,11 +75,46 @@ public class MainActivity extends AppCompatActivity {
         textView3 = findViewById(R.id.text_view3);
         textView4 = findViewById(R.id.text_view4);
         textView5 = findViewById(R.id.text_view5);
-
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        // smtp.googlemail.com
+        properties.put("mail.smtp.port", "587");
 
         //initalize fusedLocationProviderClient
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
         btLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Log.d("Areej tag", "statrt on click :" + toString());
+//
+//                //check premeisson
+//                if (ActivityCompat.checkSelfPermission(MainActivity.this,
+//                        Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+//                    //when premission graned
+//                    getLocation();
+////                    sendEmail();
+//                } else {
+//                    //when permission denied
+//                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+//                }
+            }
+        });
+
+
+
+
+
+
+
+
+        //Sender email credential
+        sEmail = "LightOnApplication@gmail.com";
+        // char[] sPassword = {'p','a','s','s'};
+        sPassword = "AAaa@@22";
+
+
+        btSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d("Areej tag", "statrt on click :" + toString());
@@ -135,62 +124,52 @@ public class MainActivity extends AppCompatActivity {
                         Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
                     //when premission graned
                     getLocation();
+//                    sendEmail();
                 } else {
                     //when permission denied
                     ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
                 }
-            }
-        });
-
-
-        //Sender email credential
-        sEmail = "raghadalfheed76@gmail.com";
-        // char[] sPassword = {'p','a','s','s'};
-        String sPassword = "Raghadalfheed_7676";
-
-
-        btSend.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Properties properties = new Properties();
-                properties.put("mail.smtp.auth", "true");
-                properties.put("mail.smtp.starttls.enable", "true");
-                properties.put("mail.smtp.host", "smtp.gmail.com");
-                // smtp.googlemail.com
-                properties.put("mail.smtp.port", "587");
+//                Properties properties = new Properties();
+//                properties.put("mail.smtp.auth", "true");
+//                properties.put("mail.smtp.starttls.enable", "true");
+//                properties.put("mail.smtp.host", "smtp.gmail.com");
+//                // smtp.googlemail.com
+//                properties.put("mail.smtp.port", "587");
                 // properties.put("mail.smtp.user", sEmail);
                 // properties.put("mail.smtp.socketFactory.fallback", "fallback");
 //                properties.put("mail.smtp.EnableSSL.enable", "true");
 //                properties.put("mail.smtp.socketFactory.port", "465");
 //                properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 
-                javax.mail.Session session = javax.mail.Session.getInstance(properties, new javax.mail.Authenticator() {
-                    @Override
-                    protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
-                        return new javax.mail.PasswordAuthentication(sEmail, sPassword);
-                    }
-                });
+//                javax.mail.Session session = javax.mail.Session.getInstance(properties, new javax.mail.Authenticator() {
+//                    @Override
+//                    protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
+//                        return new javax.mail.PasswordAuthentication(sEmail, sPassword);
+//                    }
+//                });
 
 
                 try {
-                    //Initialize email content
-                    javax.mail.Message message = new MimeMessage(session);
-
-                    //Sender email
-                    message.setFrom(new InternetAddress(sEmail));
-                    //recipient email
-                    message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(etTo.getText().toString().trim()));
-
-                    //Email subject
-                    message.setSubject(etSubject.getText().toString().trim());
-                    //email message
-                    message.setText(etMessage.getText().toString().trim());
+//                    //Initialize email content
+//                    javax.mail.Message message = new MimeMessage(session);
+//
+//                    //Sender email
+//                    message.setFrom(new InternetAddress(sEmail));
+//                    //recipient email
+//                    message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("etwaa69@gmail.com"));
+//
+//                    //Email subject
+//                    message.setSubject("Warning! Is your mobile lost?");
+//                    //email message
+//
+//
+//                    message.setText("Hello dear\nWe think your phone is lost!\nthis email is sent from Light On application " +
+//                            "to provide you with your mobile location \n"+"latitude:"+locationLatitude+ ","+"\n"
+//                           +" longitude:" +locationLongitude+" ,"+"\n" +" Country:"+ locationCountry+","+"\n"
+//                            +" City:"+locationCity+","+"\n" +" Address:"+locationAddress+"\n\n"+"Thank you!");
 
                     //send email
-                    new SendMail().execute(message);
-                } catch (MessagingException e) {
-                    e.printStackTrace();
+//                    new SendMail().execute(message);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -198,6 +177,57 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+
+    public void sendEmail(){
+//        Properties properties = new Properties();
+//        properties.put("mail.smtp.auth", "true");
+//        properties.put("mail.smtp.starttls.enable", "true");
+//        properties.put("mail.smtp.host", "smtp.gmail.com");
+//        // smtp.googlemail.com
+//        properties.put("mail.smtp.port", "587");
+        // properties.put("mail.smtp.user", sEmail);
+        // properties.put("mail.smtp.socketFactory.fallback", "fallback");
+//                properties.put("mail.smtp.EnableSSL.enable", "true");
+//                properties.put("mail.smtp.socketFactory.port", "465");
+//                properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+
+        javax.mail.Session session = javax.mail.Session.getInstance(properties, new javax.mail.Authenticator() {
+            @Override
+            protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
+                return new javax.mail.PasswordAuthentication(sEmail, sPassword);
+            }
+        });
+
+
+        try {
+            //Initialize email content
+            javax.mail.Message message = new MimeMessage(session);
+
+            //Sender email
+            message.setFrom(new InternetAddress(sEmail));
+            //recipient email
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("etwaa69@gmail.com"));
+
+            //Email subject
+            message.setSubject("Warning! Is your mobile lost?");
+            //email message
+
+
+            message.setText("Hello dear\nWe think your phone is lost!\nthis email is sent from Light On application " +
+                    "to provide you with your mobile location \n"+"latitude:"+locationLatitude+ ","+"\n"
+                    +" longitude:" +locationLongitude+" ,"+"\n" +" Country:"+ locationCountry+","+"\n"
+                    +" City:"+locationCity+","+"\n" +" Address:"+locationAddress+"\n\n"+"Thank you!");
+
+            //send email
+            new SendMail().execute(message);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
     private void getLocation() {
@@ -241,33 +271,40 @@ public class MainActivity extends AppCompatActivity {
                         //set latitude on TextView
                         textView1.setText(Html.fromHtml("<font color='#6200EE'><b>Latitude :</b><br></font>"
                         + addresses.get(0).getLatitude()));
+
+                        locationLatitude=addresses.get(0).getLatitude();
                         textView2.setText(Html.fromHtml(
                                 "<font color='#6200EE'><b>Longitude :</b><br></font>"
                                         + addresses.get(0).getLongitude()
                         ));
+                        locationLongitude=addresses.get(0).getLongitude();
                         //Set country name
                         textView3.setText(Html.fromHtml(
                                 "<font color='#6200EE'><b>Country Name :</b><br></font>"
                                         + addresses.get(0).getCountryName()
                         ));
+                        locationCountry=addresses.get(0).getCountryName();
                         //Set locality
                         textView4.setText(Html.fromHtml(
                                 "<font color='#6200EE'><b>Locality :</b><br></font>"
                                         + addresses.get(0).getLocality()
                         ));
+                        locationCity=addresses.get(0).getLocality();
                         //set address
                         textView5.setText(Html.fromHtml(
                                 "<font color='#6200EE'><b>Address :</b><br></font>"
                                         + addresses.get(0).getAddressLine(0)
                         ));
-
+                        locationAddress=addresses.get(0).getAddressLine(0);
                         Log.d("Areej tag", "Yaaaaaaaay :" + toString());
-
+                        sendEmail();
                     } catch (IOException e) {
                         Log.d("Areej tag", "ERRRROR raghaaaaaaaad :" + toString());
 
                         e.printStackTrace();
                     }
+
+
                 }
 
             }
